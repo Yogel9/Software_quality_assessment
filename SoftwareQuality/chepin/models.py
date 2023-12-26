f16ad4e88ccb8a396fc16cd71234590580cbd111
Антон Довиденков <79474789+Yogel9@users.noreply.github.com>
2023-12-26 21:46:18 +0400

    ADD Dockerfile
    - Добавлен докер файл

--- SoftwareQuality/chepin/models.py
+++ SoftwareQuality/chepin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class HolstedParam(models.Model):
+    n1 = models.IntegerField(name='Уник. операторов', validators=[MinValueValidator(0)])
+    n2 = models.IntegerField(name='Уник. операндов', validators=[MinValueValidator(0)])
+    hn1 = models.IntegerField(name='Общ. операторов', validators=[MinValueValidator(0)])
+    hn2 = models.IntegerField(name='Общ. операндов', validators=[MinValueValidator(0)])
+    k = models.IntegerField(name='Входные/выходные переменные', validators=[MinValueValidator(0)])
+
